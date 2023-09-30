900f6ca993e9fd94ab702f291dd587d740dceeaa
Denise Schirmer <denise_liima@hotmail.com>
2023-09-30 07:48:13 -0300

    Feat: component hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         light: "#c4c4c4",
         "light-bg": "#929292",
         brown: "#b07318",
+        "light-brown": "#C2B1A6",
         primary: "rgb(146, 146, 146)",
         bg: "rgb(244, 244, 255)",
       },
