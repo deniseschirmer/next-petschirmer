8fe8c384bad60567c604508725c95130cbcfba8f
deniseschirmer <deniseschirmerr@gmail.com>
2023-10-31 09:01:57 -0300

    style: adding syle for component sobre

--- src/components/Sobre.tsx
+++ src/components/Sobre.tsx
@@ -1,41 +1,29 @@
 import Image from "next/image";
 const Sobre = () => {
-  // const image1 = {
-  //   src: "/public/cat-sobre.png",
-  //   alt: "Imagem de exemplo",
-  // };
-  // const image2 = {
-  //   src: "../assets/dog1-sobre.png",
-  //   alt: "Imagem de exemplo",
-  // };
-  // const image3 = {
-  //   src: "../assets/dog2-sobre.png",
-  //   alt: "Imagem de exemplo",
-  // };
   return (
     <>
-      <section className="font-nunito border border-red-500 pt-4 pb-4">
-        <div className=" p-4 border espace border-blue-500">
-          <h1 className="text-green font-bold text-sm md:text-base lg:text-3xl lg:font-normal mb-6">
+      <section className="flex flex-wrap md:flex-wrap lg:flex-nowrap font-nunito md:py-10 md:px-36 lg:px-36 lg:mt-5 gap-11 xl:gap-24">
+        <div className=" p-4 xl:p-10">
+          <h1 className="text-green font-bold text-sm md:text-base lg:text-3xl mb-6">
             Somos o Pet Schirmer
           </h1>
 
-          <p className="mb-4 text-sm md:text-base lg:text-lg font-extrabold">
+          <p className="mb-4 text-sm md:text-base lg:text-lg font-normal leading-7">
             Com mais de 12 anos de experiência, cuidamos do seu pet com carinho
             e dedicação, oferecendo uma variedade de produtos e serviços de
             qualidade.
           </p>
-          <p className="mb-4 font-normal text-sm md:text-base lg:text-lg">
+          <p className="mb-4 font-normal text-sm md:text-base lg:text-lg leading-7">
             Nossa loja é um ambiente seguro, onde você pode se sentir tranquilo
             ao trazer seu amiguinho de estimação.
           </p>
-          <p className="font-normal text-sm md:text-base lg:text-lg">
+          <p className="font-normal text-sm md:text-base lg:text-lg leading-7">
             Conte conosco para cuidar do seu pet e proporcionar uma experiência
             confiável e satisfatória.
           </p>
         </div>
-        <div className=" p-4 flex border border-green">
-          <div className="border border-red-500 flex justify-center items-center h-64 mr-2.5">
+        <div className=" p-4 flex w-full items-center justify-center">
+          <div className="flex justify-center items-center h-64 mr-2.5">
             <Image
               src="/dog1-sobre.png"
               alt="Foto de Cachorro 1"
@@ -44,7 +32,7 @@ const Sobre = () => {
             />
           </div>
 
-          <div className="border border-yellow-500 ">
+          <div className=" ">
             <Image
               src="/dog2-sobre.png"
               alt="gatinho"
