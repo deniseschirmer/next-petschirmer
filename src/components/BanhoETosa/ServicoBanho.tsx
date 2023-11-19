8b1b60e4675f58ff44b02d4dea387baa1c1bc1bb
Gabriel-Vict0r <victorgabriel1730@gmail.com>
2023-11-19 00:34:07 -0300

    style: the component BanhoETosa is adapted for desktop screen

--- src/components/BanhoETosa/ServicoBanho.tsx
+++ src/components/BanhoETosa/ServicoBanho.tsx
@@ -1,10 +1,10 @@
+"use client";
 import Image from "next/image";
-import { FaCheck } from "react-icons/fa6";
 import CardWork from "./CardWork";
 import { care, works } from "@/utils/contents/BanhoETosa";
 import Care from "./Care";
 import Paragraph from "./Paragraph";
-import bgImage from "../../../public/banho-dog.png";
+import { MagicMotion } from "react-magic-motion";
 const ServicoBanho = () => {
   return (
     <div className="p-4">
@@ -20,30 +20,34 @@ const ServicoBanho = () => {
           />
           <h2 className="text-xl font-bold md:text-3xl">Banho e tosa </h2>
         </div>
-        <div className="flex flex-col gap-4">
-          <Paragraph>
-            Aqui no Pet Schirmer o bem-estar do seu pet é nossa prioridade,
-            garantimos que ele se sentirá confortável e seguro durante todo o
-            processo.
-          </Paragraph>
-          <Paragraph>
-            Nossa equipe experiente e dedicada, está pronta para oferecer os
-            cuidados e atenção que seu pet merece.
-          </Paragraph>
-          {/**Mapeia o objeto care(cuidados) para criar os respectivos componentes*/}
-          <ol className="flex flex-col gap-4">
-            {care.map((element) => (
-              <Care key={element.id} description={element.description} />
-            ))}
-          </ol>
+        <div className="flex flex-col lg:flex-row lg:gap-16">
+          <div>
+            <div className="flex flex-col gap-4">
+              <Paragraph>
+                Aqui no Pet Schirmer o bem-estar do seu pet é nossa prioridade,
+                garantimos que ele se sentirá confortável e seguro durante todo
+                o processo.
+              </Paragraph>
+              <Paragraph>
+                Nossa equipe experiente e dedicada, está pronta para oferecer os
+                cuidados e atenção que seu pet merece.
+              </Paragraph>
+              {/**Mapeia o objeto care(cuidados) para criar os respectivos componentes*/}
+              <ol className="flex flex-col gap-4">
+                {care.map((element) => (
+                  <Care key={element.id} description={element.description} />
+                ))}
+              </ol>
+            </div>
+          </div>
+          <Image
+            src="/banho-dog.png"
+            width={656}
+            height={457}
+            alt="cachorro tomando banho"
+            className="w-full mt-3 rounded-tr-[5rem] rounded-bl-[5rem] lg:w-1/2"
+          />
         </div>
-        <Image
-          src="/banho-dog.png"
-          width={656}
-          height={457}
-          alt="cachorro tomando banho"
-          className="w-full mt-3 rounded-tr-[5rem] rounded-bl-[5rem]"
-        />
         {/* <div
           className="bg-full bg-center w-full bg-no-repeat rounded-bl-3xl rounded-tr-3xl h-[13.938rem] md:w-full md:h-[28.563rem]"
           style={{
@@ -51,18 +55,18 @@ const ServicoBanho = () => {
           }}
         ></div> */}
         <hr className="h-[2px] my-5 bg-black border-1 dark:bg-gray-700" />
-        <h3 className="text-xl font-semibold mb-8 md:text-2xl">
-          Como Funciona
-        </h3>
-        <div className="flex flex-col gap-5 md:gap-5 md:flex-row md:flex-wrap">
-          {/**método criado para evitar a repetição de componentes */}
-          {works.map((work) => (
-            <CardWork
-              key={work.id}
-              id={work.id}
-              description={work.description}
-            />
-          ))}
+        <div>
+          <h3 className="text-xl font-semibold mb-8 md:text-2xl">
+            Como Funciona
+          </h3>
+          <div className="flex flex-col gap-5 md:gap-5 md:flex-row md:flex-wrap xl:flex-nowrap">
+            {/**método criado para evitar a repetição de componentes */}
+            {works.map((work) => (
+              <MagicMotion key={work.id}>
+                <CardWork id={work.id} description={work.description} />
+              </MagicMotion>
+            ))}
+          </div>
         </div>
       </section>
     </div>
