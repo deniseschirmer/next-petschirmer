65fd656186e821bd8796ad19cf62bb7c90fc7336
deniseschirmer <deniseschirmerr@gmail.com>
2023-11-20 05:51:04 -0300

    feat: form validation contact page

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,7 +1,61 @@
-import Footer from "@/components/Footer";
+"use client";
+import { useState, ChangeEvent, FormEvent } from "react";
 import Image from "next/image";
 
-const Page = () => {
+interface FormData {
+  nome: string;
+  telefone: string;
+  mensagem: string;
+}
+
+const Contato = () => {
+  const [formData, setFormData] = useState<FormData>({
+    nome: "",
+    telefone: "",
+    mensagem: "",
+  });
+
+  const [errors, setErrors] = useState<Partial<FormData>>({});
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // Validação - todos os campos são obrigatórios
+    const newErrors: Partial<FormData> = {};
+    if (!formData.nome.trim()) {
+      newErrors.nome = "Campo obrigatório";
+    }
+    if (!formData.telefone.trim()) {
+      newErrors.telefone = "Campo obrigatório";
+    } else if (!/^\d+$/.test(formData.telefone)) {
+      newErrors.telefone = "Digite apenas números";
+    }
+    if (!formData.mensagem.trim()) {
+      newErrors.mensagem = "Campo obrigatório";
+    }
+
+    // Adicione mais lógica de validação conforme necessário
+
+    if (Object.keys(newErrors).length === 0) {
+      // Formulário válido, pode enviar os dados
+      console.log("Dados do formulário:", formData);
+      // Adicione aqui a lógica para enviar os dados para o backend ou fazer o que for necessário
+    } else {
+      // Atualiza o estado com os erros encontrados
+      setErrors(newErrors);
+    }
+  };
+
   return (
     <>
       <section className="flex flex-col p-4 md:p-12 mx-auto font-nunito md:py-10 md:px-36 lg:px-36 lg:mt-5 gap-6 xl:gap-24">
@@ -15,34 +69,44 @@ const Page = () => {
               tirar suas dúvidas, agendar serviços e consultas. Estamos prontos
               para ajudar.
             </p>
-            <form action="flex">
+            <form onSubmit={handleSubmit}>
               <div className="">
                 <input
                   className="w-full h-[58px] t-72 rounded-lg border-2 py-1 px-4 my-2 gap-2 pl-10"
                   type="text"
                   name="nome"
-                  id=""
+                  value={formData.nome}
+                  onChange={handleChange}
                   placeholder="Nome"
                 />
+                {errors.nome && <p className="text-red-500">{errors.nome}</p>}
               </div>
               <div className="">
                 <input
                   className="w-full h-[58px] t-72 rounded-lg border-2 py-1 px-4 my-2 gap-2 pl-10"
                   type="text"
                   name="telefone"
-                  id=""
+                  value={formData.telefone}
+                  onChange={handleChange}
                   placeholder="Telefone"
                 />
+                {errors.telefone && (
+                  <p className="text-red-500">{errors.telefone}</p>
+                )}
               </div>
               <div className="">
                 <textarea
                   className="w-full h-[120px] t-72 rounded-lg border-2 py-1 px-4 my-2 gap-2 pl-10"
                   name="mensagem"
-                  id=""
+                  value={formData.mensagem}
+                  onChange={handleChange}
                   placeholder="Mensagem"
                 ></textarea>
+                {errors.mensagem && (
+                  <p className="text-red-500">{errors.mensagem}</p>
+                )}
               </div>
-              <button className="flex bg-dark-green rounded-md text-white w-80 h-9 md:w-[696px] lg:w-full lg:h-11 text-sm md:text-base items-center justify-center p-1 gap-2 my-6 ">
+              <button className="flex bg-dark-green rounded-md text-white w-80 h-9 md:w-[696px] lg:w-full lg:h-11 text-sm md:text-base items-center justify-center p-1 gap-2 my-6">
                 Enviar
               </button>
             </form>
@@ -58,9 +122,8 @@ const Page = () => {
           </div>
         </div>
       </section>
-      <Footer />
     </>
   );
 };
 
-export default Page;
+export default Contato;
