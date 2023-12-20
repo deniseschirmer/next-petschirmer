4dcffa819b14c591a1c064e33660d609494aebd5
deniseschirmer <deniseschirmerr@gmail.com>
2023-12-19 21:43:05 -0300

    fix: fixing consultorio page

--- src/app/consultorio/page.tsx
+++ src/app/consultorio/page.tsx
@@ -5,14 +5,10 @@ import ContentBoxText from "@/components/ContentBoxText";
 const page = () => {
   return (
     <>
-      <div className="flex flex-col p-4 md:p-12 mx-auto">
+      <div className="flex flex-col p-4 md:p-12 mx-auto md:flex-wrap lg:flex-nowrap font-nunito md:py-10 md:px-6 lg:px-12 lg:mt-5 gap-6 xl:gap-12">
         <Title />
 
-<<<<<<< HEAD
         <div className="flex w-full flex-col-reverse gap-8 xl:flex-row xl:px-5 mt-8 justify-center items-center">
-=======
-        <div className="flex flex-col-reverse gap-8 xl:flex-row xl:px-5 mt-8 justify-between items-start">
->>>>>>> 549c7def090215e9955bf4947c0ca6cdcd481c7d
           <ContentBoxText />
           <ContentBoxImage />
         </div>
